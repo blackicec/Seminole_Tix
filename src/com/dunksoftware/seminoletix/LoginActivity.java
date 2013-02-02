e6ae7ed9c1a3efc0850b7b59b9fa31f7a238df39
James Powell <blackice@BlackIce.(none)>
2013-02-02 01:33:33 -0500

    more updates

--- src/com/dunksoftware/seminoletix/LoginActivity.java
+++ src/com/dunksoftware/seminoletix/LoginActivity.java
@@ -1,15 +1,60 @@
 package com.dunksoftware.seminoletix;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity {
 
+	EditText editUsername,
+		editPassword;
+	
+	String mUserResponse,
+		mPassResponse;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		editUsername = (EditText)findViewById(R.id.UI_EditFSUID);
+		editPassword = (EditText)findViewById(R.id.UI_EditFSUPass);
+		
+		/* 
+		 * Another dirty shortcut (One time use variable and
+		 * One time use event handler
+		 */
+		((Button)findViewById(R.id.UI_CredSubmit)).setOnClickListener(
+				new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				boolean NameResult,
+					PassResult, blah;
+				
+				mUserResponse = editUsername.getText().toString();
+				mPassResponse = editPassword.getText().toString();
+				
+				NameResult = mUserResponse.equals("seminole");
+				PassResult = mPassResponse.equals("dunk");
+				
+				
+				if( NameResult && PassResult ) {
+					((TextView)findViewById(R.id.UI_TextResult)).
+						setText("Successful Login!!!!");
+				}
+				
+				else {
+					((TextView)findViewById(R.id.UI_TextResult)).
+						setText("Failed to Log into the database");
+				}
+			}
+		});
 	}
 
 	@Override
