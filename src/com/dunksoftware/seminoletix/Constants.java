87e90aa10a7b985541d874e21c44a184810a839c
James Powell <blackice@BlackIce.(none)>
2013-02-26 01:14:20 -0500

    Still working at getting the registration fixed

--- src/com/dunksoftware/seminoletix/Constants.java
+++ src/com/dunksoftware/seminoletix/Constants.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 
 public class Constants {
 	
-	public static String UsersAddress = "http://remote.erdaniels.com/users";
+	public static String UsersAddress = "http://chi.erdaniels.com/users";
 	public static int CARD_NUMBER_LENGTH = 12;
 	
 	public static String convertStreamToString(InputStream is) {
--- src/com/dunksoftware/seminoletix/UserControl.java
+++ src/com/dunksoftware/seminoletix/UserControl.java
@@ -53,6 +53,9 @@ public class UserControl {
 
 		@Override
 		protected String doInBackground(Void... arg0) {
+			String returnValue = "NOOP";
+
+			JSONObject[] jsonObjects;
 
 			HttpResponse response = null;
 
@@ -60,47 +63,91 @@ public class UserControl {
 			HttpClient httpclient = new DefaultHttpClient();
 			HttpPost httppost = new HttpPost(Constants.UsersAddress);
 
+
+			returnValue += " -> Made it to try, ";
+			// Add your data
+			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+
+			nameValuePairs.add(new BasicNameValuePair("cardNum", CardNumber));
+			nameValuePairs.add(new BasicNameValuePair("pin", PIN));
+			nameValuePairs.add(new BasicNameValuePair("email", Email));
+			nameValuePairs.add(new BasicNameValuePair("password", Password));
+			
 			try {
-				// Add your data
-				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-
-				nameValuePairs.add(new BasicNameValuePair("cardNum", CardNumber));
-				nameValuePairs.add(new BasicNameValuePair("pin", PIN));
-				nameValuePairs.add(new BasicNameValuePair("email", Email));
-				nameValuePairs.add(new BasicNameValuePair("password", Password));
-
 				httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
 				// Execute HTTP Post Request
-				response = httpclient.execute(httppost);
+				response = httpclient.execute(httppost);	
 
-				// If the response does not enclose an entity, there is no need
-				// to worry about connection release
-				HttpEntity entity = response.getEntity();
+				if(response.getStatusLine().getStatusCode() == 200){
+					// Connection was established. Get the content. 
 
-				if (entity != null) {
+					HttpEntity entity = response.getEntity();
+					// If the response does not enclose an entity, there is no need
+					// to worry about connection release
+
+					if (entity != null) {
+						// A Simple JSON Response Read
+						InputStream instream = entity.getContent();
+
+						JSONArray jsonArray;
+						
+						try {
+							jsonArray = new JSONArray(Constants.convertStreamToString(instream));
+
+							// allocate space for the object array
+							jsonObjects = new JSONObject[jsonArray.length()];
+							for( int i = 0; i < jsonArray.length(); ++i) {
+								jsonObjects[i] = new JSONObject(jsonArray.optString(i));
+							}
+							returnValue = "Good";
+							return returnValue;
+						}
+
+						catch (JSONException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+
+						// Close the stream.
+						instream.close();
+					}
+				}
+				else {
+					// code here for a response other than 200.  A response 200 means the webpage was ok
+					// Other codes include 404 - not found, 301 - redirect etc...
+					// Display the response line.
+					returnValue = "Unable to load page - " + response.getStatusLine();
+					return returnValue;
+				}
+
+				/*if (entity != null) {
 					// A Simple JSON Response Read
 					InputStream instream = entity.getContent();
-
+					returnValue += " Entity not NULL";
 					try {
+						returnValue += " Made it to try 2";
 						JSONArray jsonArray = new JSONArray(Constants.convertStreamToString(instream));
+						returnValue += "At return 1";
+
 						return jsonArray.toString();
+						//returnValue = jsonArray.toString();
 					} catch (JSONException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-				}
+				}*/
 
-				return Integer.toString(response.getStatusLine().getStatusCode());
+				return returnValue;
 
-			} catch (ClientProtocolException e) {
-				// TODO Auto-generated catch block
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
 			}
-			return "Error Occurred";
+			catch (IOException  ex) {
+				// Connection was not established
+				returnValue = "Connection failed; " + ex.getMessage();
+			}
+			
+			return returnValue;
 		}
-
 	}
 
 	/***
@@ -130,7 +177,7 @@ public class UserControl {
 
 			try {
 
-				// Open the webpage.
+				// Open the web page.
 				response = httpclient.execute(httpget);
 
 				if(response.getStatusLine().getStatusCode() == 200){
@@ -146,11 +193,11 @@ public class UserControl {
 
 						JSONArray jsonArray = new JSONArray(Constants.convertStreamToString(instream));
 
-						// allocate space for the obect array
+						// allocate space for the object array
 						jsonObjects = new JSONObject[jsonArray.length()];
 						for( int i = 0; i < jsonArray.length(); ++i) {
 							jsonObjects[i] = new JSONObject(jsonArray.optString(i));
-						}
+						} 
 
 						// Close the stream.
 						instream.close();
@@ -164,7 +211,6 @@ public class UserControl {
 				}
 			}
 			catch (IOException  ex) {
-				// thrown by line 80 - getContent();
 				// Connection was not established
 				returnString = "Connection failed; " + ex.getMessage();
 			}
