dcdc5f7cba0b84fc5131e51408d1bd938040b534
Eric Daniels <Eric@erdaniels.com>
2013-03-03 00:34:30 -0600

    refactoring for restServer

--- restServer/server.js
+++ restServer/server.js
@@ -1,13 +1,15 @@
 var express = require('express'),
 	mongoose = require('mongoose'),
-	models = require('./models'),
-	routes = require('./routes')
 	Session = require('connect-mongodb'),
 	check = require('validator').check,
 	https = require('https'),
 	http = require('http'),
 	fs = require('fs'),
-	User = models.User,
+	models = require('./models'),
+	routes = require('./routes'),
+	lib = require('./lib');
+
+var	User = models.User,
 	Game = models.Game,
 	LoginToken = models.LoginToken;
 
@@ -32,51 +34,6 @@ db.once('open', function callback() {
 	var session = express.session({store: new Session({db: mongoose.connection.db, maxAge: 300000}), secret: 's!p@r#o$S%^r&t' })
   	app.use(session);
 
-	function authenticateFromLoginToken(req, res, next) {
-		var cookie = JSON.parse(req.cookies.logintoken);
-
-		LoginToken.findOne({ userId: cookie.email,
-							series: cookie.series,
-							token: cookie.token }, (function(err, token) {
-			if (!token) {
-				res.send(401,'Not logged in.');
-				return;
-			}
-
-			User.findOne({ userId: token.userId }, function(err, user) {
-				if (user) {
-						req.session.user_id = user.id;
-						req.currentUser = user;
-
-						token.token = token.randomToken();
-						token.save(function() {
-							res.cookie('logintoken', token.cookieValue, { expires: new Date(Date.now() + 2 * 604800000), path: '/' });
-							next();
-						});
-				} else {
-					res.send(401, 'Not logged in.');
-				}
-			});
-		}));
-	}
-
-  	function loadUser(req, res, next) {
-		if (req.session.userId) {
-				User.findOne({'userId': req.session.userId}, function(err, user) {
-			  		if (user) {
-				    	req.currentUser = user;
-				    	next();
-				  	} else {
-				    	res.send(401,'Not logged in.');
-				  	}
-				});
-		} else if (req.cookies.logintoken) {
-			authenticateFromLoginToken(req, res, next);
-		} else {
-			res.send(401, 'Not logged in.');
-		}
-	}
-
 	app.post('/login', routes.login);
 
 	app.get('/logout', routes.logout);
@@ -88,9 +45,9 @@ db.once('open', function callback() {
 	// Games
 	app.get('/games', routes.games_view);
 
-	app.get('/game/:id', loadUser, routes.game_view);
+	app.get('/game/:id', lib.loadUser, routes.game_view);
 
-	app.post('/game/:id', loadUser, routes.game_reserve);
+	app.post('/game/:id', lib.loadUser, routes.game_reserve);
 
 	http.createServer(app).listen(80);
 	https.createServer(options, app).listen(443);
