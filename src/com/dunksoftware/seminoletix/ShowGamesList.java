93cfa64ce1ed3fb01a035dc96ed21412273937ac
James Powell <blackicec@gmail.com>
2013-04-17 23:37:47 -0400

    Almost done

--- src/com/dunksoftware/seminoletix/ShowGamesList.java
+++ src/com/dunksoftware/seminoletix/ShowGamesList.java
@@ -1,6 +1,8 @@
 package com.dunksoftware.seminoletix;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.concurrent.ExecutionException;
 
 import org.apache.http.HttpResponse;
@@ -24,63 +26,92 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class ShowGamesList extends Activity {
-	
+
 	JSONObject mUserObject;
 	JSONArray mGameIdTags;
-	
+
 	private TableLayout mainTable;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_show_games_list);
-		
+
 		TextView welcomeMsg = (TextView)findViewById(R.id.UI_GreetingText);
 		mainTable = (TableLayout)findViewById(R.id.UI_MainTableLayout);
-		
+
 		welcomeMsg.setText(ListActivity.userName);
-		
+
 		// get the list of current reserved games
 		GetCurrentUserInfo userInfo = new GetCurrentUserInfo();
 		userInfo.execute();
-		
+
 		try {
 			String serverResponse = userInfo.get();
-			
+
 			mUserObject = new JSONObject(serverResponse);
 			mGameIdTags = mUserObject.getJSONArray("tickets");
-			
-			Toast.makeText(this, serverResponse, Toast.LENGTH_LONG).show();
+
+			// get full list of games to compare against
+			String games = new GetListOfGames().execute().get();
+			JSONArray gamesArray = new JSONArray(games);
+
+			//Toast.makeText(this, games, Toast.LENGTH_LONG).show();
+
+			//Toast.makeText(this, serverResponse, Toast.LENGTH_LONG).show();
 
 			for(int i = 0; i < mGameIdTags.length(); i++) {
-				TableRow gameTableRow = new TableRow(this);
-				LinearLayout list = new LinearLayout(this);
+				for(int j = 0; j < gamesArray.length(); ++j)
+					if(mGameIdTags.getJSONObject(i).getString("game_id")
+							.equals(gamesArray.getJSONObject(j).getString("_id"))) {
 
-				TextView[] info = new TextView[4];
+						JSONObject currentObject = gamesArray.getJSONObject(j);
 
-				// set the list to a top down look
-				list.setOrientation(LinearLayout.VERTICAL);
+						TableRow gameTableRow = new TableRow(this);
+						LinearLayout list = new LinearLayout(this);
+
+						TextView[] info = new TextView[4];
+
+						// set the list to a top down look
+						list.setOrientation(LinearLayout.VERTICAL);
 
 
-				info[0] = new TextView(this);
-				list.addView(info[0]);
+						info[0] = new TextView(this);
+						info[0].setText("\t\tSport:\t\t" + currentObject.getString("sport"));
 
-				info[1] = new TextView(this);
-				list.addView(info[1]);
+						list.addView(info[0]);
 
-				info[2] = new TextView(this);
-				list.addView(info[2]);
+						info[1] = new TextView(this);
 
-				info[3] = new TextView(this);
-				list.addView(info[3]);
+						//Format the date so that it is appropriate
+						String dateTime = currentObject.getString("date");
 
-				list.setPadding(0, 5, 0, 20);
-				gameTableRow.addView(list);
-				gameTableRow.setBackgroundResource(R.drawable.img_gloss_background);
+						String date = FormatDate(dateTime);
 
-				mainTable.addView(gameTableRow);
+						info[1].setText("\t\tGame Date:\t\t" + date);
+
+						list.addView(info[1]);
+
+						info[2] = new TextView(this);
+						info[2].setText("\t\tHome Team:\t\t" + currentObject.getJSONObject("teams")
+								.getString("home").toUpperCase());
+
+						list.addView(info[2]);
+
+						info[3] = new TextView(this);
+						info[3].setText("\t\tAgainst:\t\t" + currentObject.getJSONObject("teams")
+								.getString("away").toUpperCase());
+
+						list.addView(info[3]);
+
+						list.setPadding(0, 5, 0, 20);
+						gameTableRow.addView(list);
+						gameTableRow.setBackgroundResource(R.drawable.img_gloss_background);
+
+						mainTable.addView(gameTableRow);
+					}
 			}
-			
+
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		} catch (ExecutionException e) {
@@ -89,7 +120,21 @@ public class ShowGamesList extends Activity {
 			e.printStackTrace();
 		}
 	}
-	
+
+	@SuppressWarnings("deprecation")
+	String FormatDate(String Date) {
+		String[] splits = Date.split("T");
+
+		splits = splits[0].split("-");
+
+		Date d = new Date(Integer.parseInt(splits[0]) - 1900, 
+				Integer.parseInt(splits[1]), Integer.parseInt(splits[2])); 
+		DateFormat newDate = DateFormat.getDateInstance(DateFormat.LONG); 
+		newDate.format(d);
+
+		return DateFormat.getDateInstance(DateFormat.LONG).format(d);
+	}
+
 	class GetCurrentUserInfo extends AsyncTask<Void, Void, String> {
 
 		@Override
@@ -100,7 +145,7 @@ public class ShowGamesList extends Activity {
 
 			//sets cookie
 			client.setCookieStore(UserControl.mCookie);
-			
+
 			// Prepare a request object
 			HttpGet httpget = new HttpGet(Constants.CurrentUserAddress); 
 
@@ -122,6 +167,40 @@ public class ShowGamesList extends Activity {
 			}
 			return returnString;
 		}
+	}
+
+	class GetListOfGames extends AsyncTask<Void, Void, String> {
+
+		@Override
+		protected String doInBackground(Void... params) {
+			// Create a new HttpClient and Post Header
+			MyHttpClient client = new MyHttpClient(null);
+
+			//sets cookie
+			client.setCookieStore(UserControl.mCookie);
+
+			// Prepare a request object
+			HttpGet httpget = new HttpGet(Constants.GamesAddress); 
+
+			// Execute the request
+			HttpResponse response=null;
+
+			// return string
+			String returnString = null;
+
+			try {
+				// Open the web page.
+				response = client.execute(httpget);
+				returnString = EntityUtils.toString(response.getEntity());
+
+			}
+			catch (IOException  ex) {
+				// Connection was not established
+				returnString = "Connection failed; " + ex.getMessage();
+			}
+			return returnString;
+		}
+
 	}
 
 }
